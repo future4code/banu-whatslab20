b5a092726a92ebb3b2dda7b80a207e8f5f250128
Vinicius <cvinicius@outlook.com>
2021-09-19 16:56:46 -0300

    Responsividade

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,5 +1,23 @@
 import Principal from "./pages/Principal";
 import { createGlobalStyle } from "styled-components";
+import { keyframes } from "styled-components";
+
+const animateEntrance = keyframes`
+0% {
+    -webkit-transform: scale(2);
+            transform: scale(2);
+    -webkit-filter: blur(4px);
+            filter: blur(4px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: scale(1);
+            transform: scale(1);
+    -webkit-filter: blur(0px);
+            filter: blur(0px);
+    opacity: 1;
+  }
+`
 
 const GlobalStyle = createGlobalStyle`
 * {
@@ -9,15 +27,17 @@ const GlobalStyle = createGlobalStyle`
 
 body {
   background-color: #141f29;
-  height: 50vh;
+  height: 100vh;
   box-sizing: border-box;
+  animation-name: ${animateEntrance};
+  animation-duration: 1s;
 }
-`
+`;
 
 function App() {
   return (
     <>
-      <GlobalStyle/>
+      <GlobalStyle />
       <Principal />
     </>
   );
